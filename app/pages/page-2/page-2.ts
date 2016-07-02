1dd52911a9255e1b171021b7d0c8c014268216b0
Aleksandar <vukasinovic@outlook.com>
2016-07-02 13:49:27 +0200

    Functionality added

--- app/pages/page-2/page-2.ts
+++ app/pages/page-2/page-2.ts
@@ -6,8 +6,40 @@ import {NavController, NavParams} from 'ionic-angular';
   templateUrl: 'build/pages/page-2/page-2.html'
 })
 export class Page2 {
-  	
+	time: string;
+	money: string;
+	type: string;
+
+	goButtonStatus = false;
+	goBtn = true;
+
+	startGame(money, type) {
+		if(money != undefined && type != undefined)
+		{
+			this.goButtonStatus = true;
+		}
+		/* Timer here */
+	}
+
+	changedMoney(value, type) {
+		console.log(value, type);
+		if(value != undefined && type != undefined)
+		{
+			this.goBtn = null;
+		}
+	}
+
+	changedType(value, money) {
+		console.log(value, money);
+		if(value != undefined && money != undefined)
+		{
+			this.goBtn = null;
+		}
+	}
+
   	constructor(private navController: NavController, navParams: NavParams) {
 
+  		this.goButtonStatus = false;
+  		this.goBtn = true;
 	}
 }
