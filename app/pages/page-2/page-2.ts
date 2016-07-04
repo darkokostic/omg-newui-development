ce8cbec7c53e6061993595801a48ec9ed1eab564
Aleksandar <vukasinovic@outlook.com>
2016-07-04 20:43:49 +0200

    graph dot

--- app/pages/page-2/page-2.ts
+++ app/pages/page-2/page-2.ts
@@ -10,38 +10,44 @@ export class Page2 {
 	timeLineClass: string;
 	goBtnClass: string;
 
+	randomStatus: boolean;
+
 	gameStatus: boolean;
 	time: string;
 	money: string;
 	type: string;
+	dotPosition: number = 50;
 
 	goButtonStatus = false;
 	goBtn = true;
 
-	startGame(money, type) {
+	startGame(money, type, graphDot) {
+		this.randomStatus = Math.random() >= 0.5;
 		if(money != undefined && type != undefined)
 		{
 			this.goButtonStatus = true;
 		}
 		/* Fake Losing/winning */
 	    setInterval(() => {
-	    	if(this.gameStatus == true) {
+	    	if(this.gameStatus == this.randomStatus) {
 	    		/* If user is winning here */
 	    		this.upperClass = 'winning-light';
 	    		this.timeLineClass = 'winning-dark';
 	    		this.goBtnClass = 'winning-light';
+	    		this.dotPosition  = this.dotPosition - 1;
 	    		this.gameStatus = false;
 	    	} else {
+	    		this.dotPosition  = this.dotPosition + 1;
 	    		this.gameStatus = true;
 	    		/* If user is losing here */
 	    		this.upperClass = 'losing-light';
 	    		this.timeLineClass = 'losing-dark';
 	    		this.goBtnClass = 'losing-light';
-
 	    	}
-
-	    }, 1000);
-
+	    	this.randomStatus = Math.random() >= 0.5;
+	    	graphDot.style.marginTop = this.dotPosition + "%";
+	    	
+	    }, 500);
 	}
 
 	changedMoney(value, type) {
