01a483d232222179fd9af66fb7e20f8247091a38
Darko Kostic <Darko Kostic>
2016-07-11 23:40:50 +0200

    fixed trading view

--- app/app.ts
+++ app/app.ts
@@ -20,6 +20,7 @@ class MyApp {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       StatusBar.styleDefault();
+      Splashscreen.hide();
     });
   }
 }
