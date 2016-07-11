610ec06c55f5d8ee5a4f41ac1eaf17b563b63f93
Darko Kostic <Darko Kostic>
2016-07-11 13:07:59 +0200

    iPad responsive

--- app/app.ts
+++ app/app.ts
@@ -20,7 +20,6 @@ class MyApp {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       StatusBar.styleDefault();
-      Splashscreen.hide();
     });
   }
 }
